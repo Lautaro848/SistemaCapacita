a5aeecb2da7d4dc83c75bdf71646400e8f9d1e95
Lautaro848 <lautarolaborda846@gmail.com>
2026-03-01 19:35:16 -0300

    fix: orden correcto de rutas en app.js

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-const authRoutes = require('./src/routes/authRoutes');
+
+const authRoutes      = require('./src/routes/authRoutes');
+const apiRoutes       = require('./src/routes/apiRoutes');
+const empleadosRoutes = require('./src/routes/empleadosRoutes');
+const aptitudesRoutes = require('./src/routes/aptitudesRoutes');
+const { verificarToken } = require('./src/middlewares/auth');
+
 app.use('/', authRoutes);
+app.use('/api', apiRoutes);
+app.use('/', empleadosRoutes);
+app.use('/', aptitudesRoutes);
+
 app.get('/', (req, res) => res.redirect('/login'));
 app.get('/tv', (req, res) => res.render('tv'));
-const { verificarToken } = require('./src/middlewares/auth');
 app.get('/dashboard', verificarToken, (req, res) => {
   res.render('dashboard', { usuario: req.usuario });
 });
+
 const PORT = process.env.PORT || 3000;
-
-
-const apiRoutes = require('./src/routes/apiRoutes');
-app.use('/api', apiRoutes);
-
-const empleadosRoutes = require('./src/routes/empleadosRoutes');
-app.use('/', empleadosRoutes);
-
-const aptitudesRoutes = require('./src/routes/aptitudesRoutes');
-app.use('/', aptitudesRoutes);
-
 app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
